75bdd4cf69995666d8f880c3d40a01946eda1958
Mehdi Haghgoo <powergame_coder2@yahoo.com>
2024-02-09 00:36:42 +0330

    Add backup.sh and README.md

---
+++ README.md
@@ -0,0 +1,6 @@
+# site-backup: A simple site-backup solution
+
+Run the script to create a backup in the specified directory
+
+Can be used in combination with a cron job to automate server backups
+
---
+++ backup.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+command_syntax="backup.sh source_dir dest_dir db_name"
+
+if [ $# -ne 3 ];
+then  printf "Command syntax: $command_syntax\n"
+exit 1
+fi
+
+$source_dir=$1
+$dest_dir=$2
+$db_name=$3
+
+$backup_name="$(date +%b-%d-%Y)"
+temp_dir="/tmp/$backup_name"
+
+# Make backup directory and sql sub-directory
+mkdir  -p "$temp_dir/sql"
+
+#Copy the source directory 
+copy -rv $source_dir $temp_dir
+
+# Backup the database
+mysqldump -u root $db_name > "$temp_dir/sql/$dbname.sql"
+
+# Compress the backup
+tar cvzf "/tmp/$backup_name.tar.gz" "/tmp/$backup_name" 
+
+# Delete the temporary backup folder
+rm -rf "/tmp/$backup_name"
+
+# Move the backup zip to destination directory
+mv "/tmp/$backup_name.tar.gz" $dest_dir
+
+
+
+
+
+
