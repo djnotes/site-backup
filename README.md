01392d9662877900a468c7b9c8f5258a59bd16b0
Mehdi Haghgoo <powergame_coder2@yahoo.com>
2024-02-09 00:47:21 +0330

    Fix syntax errors

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ Run the script to create a backup in the specified directory
 
 Can be used in combination with a cron job to automate server backups
 
+# Important: The command needs to be run as root
--- backup.sh
+++ backup.sh
@@ -7,21 +7,21 @@ then  printf "Command syntax: $command_syntax\n"
 exit 1
 fi
 
-$source_dir=$1
-$dest_dir=$2
-$db_name=$3
+source_dir=$1
+dest_dir=$2
+db_name=$3
 
-$backup_name="$(date +%b-%d-%Y)"
+backup_name="$(date +%b-%d-%Y)"
 temp_dir="/tmp/$backup_name"
 
 # Make backup directory and sql sub-directory
 mkdir  -p "$temp_dir/sql"
 
 #Copy the source directory 
-copy -rv $source_dir $temp_dir
+cp -rv $source_dir $temp_dir
 
 # Backup the database
-mysqldump -u root $db_name > "$temp_dir/sql/$dbname.sql"
+mysqldump -u root $db_name > "$temp_dir/sql/$db_name.sql"
 
 # Compress the backup
 tar cvzf "/tmp/$backup_name.tar.gz" "/tmp/$backup_name" 
