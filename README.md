8b8014bf207ffa401524c451b8b98eeb9f6f78d5
Mehdi Haghgoo <powergame_coder2@yahoo.com>
2024-02-22 15:35:31 +0330

    Update README

--- README.md
+++ README.md
@@ -5,3 +5,15 @@ Run the script to create a backup in the specified directory
 Can be used in combination with a cron job to automate server backups
 
 # Important: The command needs to be run as root
+
+# How to Run with Cron
+- Install the script somewhere like /usr/local/bin:
+```
+sudo install ./backup.sh /usr/local/bin/backup
+```
+- Create a cronjob that will execute the script for every interval you want to back up a specific folder.
+```
+crontab -e
+# TO BE COMPLETED
+
+```
--- backup.sh
+++ backup.sh
@@ -2,9 +2,9 @@
 
 command_syntax="backup.sh source_dir dest_dir db_name"
 
-if [ $# -ne 3 ];
-then  printf "Command syntax: $command_syntax\n"
-exit 1
+if [ $# -ne 3 ]; then
+  printf "Command syntax: $command_syntax\n"
+  exit 1
 fi
 
 source_dir=$1
@@ -15,25 +15,27 @@ backup_name="$(date +%b-%d-%Y)"
 temp_dir="/tmp/$backup_name"
 
 # Make backup directory and sql sub-directory
-mkdir  -p "$temp_dir/sql"
+printf "Creating backup directory...\n";
+mkdir -p "$temp_dir/sql" || { printf "Failed to create temporary directory: $temp_dir\n"; exit 1; }
 
-#Copy the source directory 
-cp -rv $source_dir $temp_dir
+# Copy the source directory
+printf "Copying the source directory...\n";
+cp -rv "$source_dir" "$temp_dir" || { printf "Failed to copy source directory: $source_dir\n"; exit 1; }
 
 # Backup the database
-mysqldump -u root $db_name > "$temp_dir/sql/$db_name.sql"
+printf "Backing up database...\n";
+mysqldump -u root "$db_name" > "$temp_dir/sql/$db_name.sql" || { printf "Failed to create database backup: $db_name\n"; exit 1; }
 
 # Compress the backup
-tar cvzf "/tmp/$backup_name.tar.gz" "/tmp/$backup_name" 
+printf "Compressing the backup...\n";
+tar cvzf "/tmp/$backup_name.tar.gz" "/tmp/$backup_name" || { printf "Failed to create compressed backup: $backup_name.tar.gz\n"; exit 1; }
 
 # Delete the temporary backup folder
-rm -rf "/tmp/$backup_name"
+printf "Deleting the temporary backup folder...\n";
+rm -rf "$temp_dir" || { printf "Failed to delete temporary directory: $temp_dir\n"; exit 1; }
 
 # Move the backup zip to destination directory
-mv "/tmp/$backup_name.tar.gz" $dest_dir
+printf "Moving the backup archive to destination directory...\n";
+mv "/tmp/$backup_name.tar.gz" "$dest_dir" || { printf "Failed to move backup file: $backup_name.tar.gz\n"; exit 1; }
 
-
-
-
-
-
+echo "Backup completed successfully!"
